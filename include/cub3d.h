0b6f5b8bbca3ac4ed8a4e8e37206b1723918f1e6
Thomas Nakas <tnakas@2-E-8.42heilbronn.de>
2024-09-01 23:48:20 +0200

    adding some draft structs that wil give us a dirrection

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 11:28:13 by sganiev           #+#    #+#             */
-/*   Updated: 2024/08/30 19:52:23 by tnakas           ###   ########.fr       */
+/*   Updated: 2024/09/01 23:47:40 by tnakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,46 @@
 # include <stdbool.h>
 # include "../mlx42/include/MLX42/MLX42.h"
 # include "../libft/libft.h"
+
+typedef struct s_player
+{
+	int			x;
+	int			y;
+	bool		right;
+	bool		left;
+	bool		forward;
+	bool		backward;
+	double		angle;
+	bool		rot_right;
+	bool		rot_left;
+	bool		rot_up;
+	bool		rot_down;
+	float		view_plane;
+}	t_player;
+
+typedef struct s_ray
+{
+	double	angle;
+	double	dist;
+	bool	is_wall;
+}	t_ray;
+
+typedef struct s_map
+{
+	char	**map_array_input;
+	int		width;
+	int		height;
+	int		x_pos_player;
+	int		y_pos_player;
+}	t_map;
+
+typedef struct s_cub3d
+{
+	mlx_image_t	*img;
+	mlx_t		*mlx_p;
+	t_ray		*ray;
+	t_map		*map;
+	t_player	*player;
+}	t_cub3d;
 
 #endif
